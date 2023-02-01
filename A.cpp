be3caecda33cfcb7df072ea3d80072f6f91987d3
kusano <kusano@users.noreply.github.com>
2023-02-02 06:06:26 +0900

    焼きなまし
    39292621530

--- A.cpp
+++ A.cpp
@@ -2,7 +2,10 @@
 #include <vector>
 #include <queue>
 #include <utility>
+#include <chrono>
+#include <cmath>
 using namespace std;
+using namespace std::chrono;
 
 const long long oo = 1'000'000'000LL;
 
@@ -18,6 +21,7 @@ struct City
     vector<int> R;
     vector<vector<vector<long long>>> dist;
     vector<long long> F; // *N(N-1)
+    vector<int> C;
 
     City(int N, int M, int D, int K, const vector<int> &U, const vector<int> &V, const vector<long long> &W)
         : N(N)
@@ -30,6 +34,7 @@ struct City
         , R(M)
         , dist(D+1, vector<vector<long long>>(N, vector<long long>(N)))
         , F(D+1)
+        , C(D)
     {
         for (int i=0; i<M; i++)
         {
@@ -39,11 +44,26 @@ struct City
             Ei[V[i]].push_back(i);
 
             R[i] = i%D;
+            C[R[i]]++;
         }
 
         update_dist(D);
         for (int d=0; d<D; d++)
             update_dist(d);
+    }
+
+    void set(int m, int d)
+    {
+        if (R[m]==d)
+            return;
+
+        C[R[m]]--;
+        int old = R[m];
+        R[m] = d;
+        C[R[m]]++;
+
+        update_dist(old);
+        update_dist(d);
     }
 
     void update_dist(int k)
@@ -66,11 +86,15 @@ struct City
             while (!Q.empty())
             {
                 int x = Q.top().second;
+                long long qd = -Q.top().second;
                 Q.pop();
 
                 if (F[x]!=0)
                     continue;
                 F[x] = 1;
+
+                if (qd>dist[k][p][x])
+                    continue;
 
                 for (int i=0; i<(int)E[x].size(); i++)
                 {
@@ -107,6 +131,44 @@ struct City
     }
 };
 
+const double TIME = 5.0;
+
+const int expN = 1024;
+const double expX = 16;
+int expT[expN];
+
+void my_exp_init()
+{
+    for (int x=0; x<expN; x++)
+    {
+        double x2 = (double)-x/expN*expX;
+        double e = exp(x2)*0x80000000+.5;
+        if (e>=0x7fffffff)
+            expT[x] = 0x7fffffff;
+        else
+            expT[x] = int(e);
+    }
+}
+
+//  exp(t)*0x80000000;
+int my_exp(double x)
+{
+    int x2 = int(x/-expX*expN+.5);
+    if (x2<0)
+        return expT[0];
+    if (x2>=expN)
+        return expT[expN-1];
+    return expT[x2];
+}
+
+int xor64() {
+    static uint64_t x = 88172645463345263ULL;
+    x ^= x<<13;
+    x ^= x>> 7;
+    x ^= x<<17;
+    return int(x&0x7fffffff);
+}
+
 int main()
 {
     int N, M, D, K;
@@ -123,9 +185,67 @@ int main()
     for (int i=0; i<N; i++)
         cin>>X[i]>>Y[i];
 
+    system_clock::time_point start = system_clock::now();
+
     City city(N, M, D, K, U, V, W);
 
-    cerr<<"Score: "<<city.calc_score()<<endl;
+    my_exp_init();
+
+    long long score = city.calc_score();
+    long long best_score = score;
+    vector<int> best_R = city.R;
+
+    double temp_inv;
+    int iter;
+    for (iter=0; ; iter++)
+    {
+        if (true)
+        {
+            system_clock::time_point now = system_clock::now();
+            double time = chrono::duration_cast<chrono::nanoseconds>(now-start).count()*1e-9/TIME;
+            if (time>1.0)
+                break;
+            double temp = 1e6*(1.0-time);
+            temp_inv = 1./temp;
+        }
+
+        int m;
+        int d;
+        while (true)
+        {
+            m = xor64()%M;
+            d = xor64()%D;
+
+            if (city.R[m]!=d && city.C[d]<K)
+                break;
+        }
+
+        int old_d = city.R[m];
+        city.set(m, d);
+
+        long long score2 = city.calc_score();
+
+        if (score2<score ||
+            //exp((score-score2)*temp_inv)*0x80000000>xor64())
+            my_exp((score-score2)*temp_inv)>xor64())
+        {
+            score = score2;
+
+            if (score>best_score)
+            {
+                best_score = score;
+                best_R = city.R;
+            }
+        }
+        else
+        {
+            city.set(m, old_d);
+        }
+    }
+
+    cerr<<"Iteration: "<<iter<<endl;
+    cerr<<"Score: "<<best_score<<endl;
+    cerr<<"Time: "<<chrono::duration_cast<chrono::nanoseconds>(system_clock::now()-start).count()*1e-9<<endl;
 
     for (int i=0; i<M; i++)
         cout<<(i==0?"":" ")<<city.R[i]+1;
